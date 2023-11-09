9a165602c7e4c214b05eb543e298b37bf867738e
PaKre66 <pawel.pazdur0@gmail.com>
2023-11-09 23:14:22 +0100

    nwm
    sjkxghsh

--- strona prepisu/script.js
+++ strona prepisu/script.js
@@ -1,1 +1,24 @@
-// być może wysówany aside
+// // być może wysówany aside
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const toggleButton = document.getElementById('toggleButton');
+//     const article = document.querySelector('article');
+//     const aside = document.querySelector('aside');
+  
+//     toggleButton.addEventListener('click', function () {
+//       const asideWidth = 250; // Szerokość aside w pikselach
+  
+//       if (article.style.marginRight === `${asideWidth}px`) {
+//         // Jeśli aside jest już wysunięte, schowaj je
+//         article.style.marginRight = '0';
+//         aside.style.marginRight = `-${asideWidth}px`;
+//       } else {
+//         // Jeśli aside jest schowane, wysuń je
+//         article.style.marginRight = `${asideWidth}px`;
+//         aside.style.marginRight = '0';
+//       }
+//     });
+//   });
+  
+  
