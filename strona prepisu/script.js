80097a6155b48dd5f9fe7d928f101e7bfee00324
PaKre66 <pawel.pazdur0@gmail.com>
2023-11-15 18:40:46 +0100

    "Tylko ten nie popełnia błędów, kto nic nie robi"

--- strona prepisu/script.js
+++ strona prepisu/script.js
@@ -1,24 +1,28 @@
-// // być może wysówany aside
 
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const article = document.querySelector('article');
+    const aside = document.querySelector('aside');
+  
+    toggleButton.addEventListener('click', function () {
+      const asideWidth = 250; // Szerokość aside w pikselach
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const toggleButton = document.getElementById('toggleButton');
-//     const article = document.querySelector('article');
-//     const aside = document.querySelector('aside');
-  
-//     toggleButton.addEventListener('click', function () {
-//       const asideWidth = 250; // Szerokość aside w pikselach
-  
-//       if (article.style.marginRight === `${asideWidth}px`) {
-//         // Jeśli aside jest już wysunięte, schowaj je
-//         article.style.marginRight = '0';
-//         aside.style.marginRight = `-${asideWidth}px`;
-//       } else {
-//         // Jeśli aside jest schowane, wysuń je
-//         article.style.marginRight = `${asideWidth}px`;
-//         aside.style.marginRight = '0';
-//       }
-//     });
-//   });
-  
+      
+      if (article.style.marginRight === `${asideWidth}px`) {
+        // Jeśli aside jest już wysunięte, schowaj je
+        article.style.marginRight = '0';
+        article.style.transition='margin-right 1s ease-in-out';
+        aside.style.marginRight = `-${asideWidth}px`;
+        
+      } else {
+        // Jeśli aside jest schowane, wysuń je
+        article.style.marginRight = `${asideWidth}px`;
+        aside.style.marginRight = '0';
+        aside.style.transition='margin-right 1s ease-in-out';
+
+      }
+    });
+  });
+  // trzeba dodać addEventListener żeby wykrywał koniec 
+  // animacji i tam display none przy schowanym a przy wysuniętym block
   
