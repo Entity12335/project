2707e0f0a8ad9993cebde6e994a6cdca3b560cda
PaKre66 <pawel.pazdur0@gmail.com>
2024-01-10 21:52:30 +0100

    sss

--- strona prepisu/script.js
+++ strona prepisu/script.js
@@ -1,28 +1,9 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
-    const article = document.querySelector('article');
-    const aside = document.querySelector('aside');
-  
-    toggleButton.addEventListener('click', function () {
-      const asideWidth = 250; // Szerokość aside w pikselach
-
-      
-      if (article.style.marginRight === `${asideWidth}px`) {
-        // Jeśli aside jest już wysunięte, schowaj je
-        article.style.marginRight = '0';
-        article.style.transition='margin-right 1s ease-in-out';
-        aside.style.marginRight = `-${asideWidth}px`;
-        
-      } else {
-        // Jeśli aside jest schowane, wysuń je
-        article.style.marginRight = `${asideWidth}px`;
-        aside.style.marginRight = '0';
-        aside.style.transition='margin-right 1s ease-in-out';
-
+window.addEventListener("DOMContentLoaded",()=>{
+  let x = document.forms.search
+  x.addEventListener("submit",(event)=>{
+      event.preventDefault()
+          if(x.Szukaj.value == "rick"){
+          document.querySelector("#rick").style.display = "block"
       }
-    });
-  });
-  // trzeba dodać addEventListener żeby wykrywał koniec 
-  // animacji i tam display none przy schowanym a przy wysuniętym block
-  
+      })
+})
