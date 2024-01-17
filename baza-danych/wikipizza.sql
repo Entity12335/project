7380a849baf6a21c595c8f37b063050e57fd48c1
Entity12335 <tomasz.skupien@onet.pl>
2024-01-17 23:41:03 +0100

    baza 0.1

--- baza-danych/wikipizza.sql
+++ baza-danych/wikipizza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sty 03, 2024 at 03:22 PM
+-- Generation Time: Sty 17, 2024 at 11:40 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `artykuły`
+--
+
+CREATE TABLE `artykuły` (
+  `IDart` int(11) NOT NULL,
+  `Tytuł` text NOT NULL,
+  `Opis` mediumtext NOT NULL,
+  `Składniki` longtext NOT NULL,
+  `Img` longblob NOT NULL,
+  `Przepis` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `users`
 --
 
@@ -32,18 +47,24 @@ CREATE TABLE `users` (
   `Login` text NOT NULL,
   `Hasło` text NOT NULL,
   `Email` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci COMMENT='użytkownicy';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`ID`, `Login`, `Hasło`, `Email`) VALUES
-(1, 'Entity_K', '1234', 'boży.dar@gamil.com');
+(1, 'TESTER', '$2y$10$FCMLiPGOlQybHTxteJkeR.CkkPZ64ouXM8E/QPsy65H7STA3BWQk2', 'Bozy.dar@gmail.com');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `artykuły`
+--
+ALTER TABLE `artykuły`
+  ADD PRIMARY KEY (`IDart`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -56,10 +77,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `artykuły`
+--
+ALTER TABLE `artykuły`
+  MODIFY `IDart` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
