d70d779d067e2748b7373d5ad1c525bef7b66da9
PaKre66 <113010144+PaKre66@users.noreply.github.com>
2024-01-25 09:14:19 +0100

    pomysły

--- strona prepisu/lista.js
+++ strona prepisu/lista.js
@@ -1,11 +1,29 @@
-function addFormField() {
-    var form = document.getElementById("myForm");
-    var ul = form.querySelector("ul");
 
-    var newLi = document.createElement("li");
-    newLi.innerHTML = `
-    
-    <input type="text" placeholder="składniki" name="składnik" required>`;
-    ul.appendChild(newLi);
-    left.style.height = (ul.offsetHeight + 5) + "vh";
-}
+        function addFormField() {
+            
+            var form = document.getElementById("myForm");
+            var ul = form.querySelector("ul");
+
+            var newLi = document.createElement("li");
+            newLi.innerHTML = `
+            
+            <input type="text" placeholder="składniki" name="składnik" required>`;
+            ul.appendChild(newLi);
+            // left.style.height = (ul.offsetHeight + 5) + "vh";
+        }
+
+        //przycisk do usuwania elementu
+        // function removeFormField() {
+            
+        //     var form = document.getElementById("myForm");
+        //     var ul = form.querySelector("ul");
+
+        //     var newLi = document.removeChild("li");
+           
+        //     ul.appendChild(newLi);
+            
+        // }
+        // do podglądu obrazu
+        //scieżka ma byc z value z wybranego elementu
+// let sciezka = '';
+// #podgladObrazu.style.backgroundImage = sciezka;
