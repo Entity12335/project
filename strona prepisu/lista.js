8471e29d8279ecdc21400724290db23fdfcfb390
PaKre66 <pawel.pazdur0@gmail.com>
2024-01-28 18:00:00 +0100

    już lista działa (prawie)
    jeszcze usuwanie elementu i lewy aside jest upośledzony

--- strona prepisu/lista.js
+++ strona prepisu/lista.js
@@ -1,35 +1,27 @@
 
 
+//dodawanie elementu
+function addFormField() {
+        
+    var form = document.getElementById("myForm");
+    var ul = form.querySelector("ul");
+    var newLi = document.createElement("li");
+    newLi.innerHTML = `
+    <input type="text" placeholder="składniki" name="składnik" required>
+    <span class="remove-btn" onclick="removeFormField(this)">usuń składnik</span>
+    `;
+    ul.appendChild(newLi);
+    };
 
-        function addFormField() {
-            
-            var form = document.getElementById("myForm");
-            var ul = form.querySelector("ul");
-
-            var newLi = document.createElement("li");
-            newLi.innerHTML = `
-            
-            <input type="text" placeholder="składniki" name="składnik" required>`;
-            ul.appendChild(newLi);
-            let wysokość = document.getElementsByClassName("left");
-            // console.log(wysokość);
-            wysokość.style.height+='5vh';
-            
-        }
-
-        //przycisk do usuwania elementu
-        // function removeFormField() {
-            
-        //     var form = document.getElementById("myForm");
-        //     var ul = form.querySelector("ul");
-
-        //     var newLi = document.removeChild("li");
-           
-        //     ul.appendChild(newLi);
-            
-        // }
-        // do podglądu obrazu
-        //scieżka ma byc z value z wybranego elementu
+//przycisk do usuwania elementu
+    function removeFormField(element) {
+        var form = document.getElementById("myForm");
+        var ul = form.querySelector("ul");
+        var li = element.parentNode;
+        ul.removeChild(li);
+    }
+// do podglądu obrazu
+//scieżka ma byc z value z wybranego elementu
 // let sciezka = '';
 // #podgladObrazu.style.backgroundImage = sciezka;
 
