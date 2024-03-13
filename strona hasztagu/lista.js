f8552a6819cc632ee58df358b33dbdc8c312d4a2
PaKre66 <pawel.pazdur0@gmail.com>
2024-03-13 19:13:19 +0100

    cóś

--- strona hasztagu/lista.js
+++ strona hasztagu/lista.js
@@ -4,22 +4,25 @@
 function addFormField() {
         
     var form = document.getElementById("myForm");
-    var ul = form.querySelector("ul");
-    var newLi = document.createElement("li");
-    newLi.innerHTML = `
-    <input type="text" placeholder="składniki" name="składnik" required>
-    <span class="remove-btn" onclick="removeFormField(this)">usuń składnik</span>
+    var section = form.querySelector("hasz");
+    var newA = document.createElement("hasztag");
+    newA.innerHTML = `
+    <a href="../strona prepisu/index.html"  class="hasztag" >  
+                    <div>
+                    #hasz1
+                    </div>
+                </a>
     `;
-    ul.appendChild(newLi);
+    section.appendChild(newA);
     };
 
 //przycisk do usuwania elementu
-    function removeFormField(element) {
-        var form = document.getElementById("myForm");
-        var ul = form.querySelector("ul");
-        var li = element.parentNode;
-        ul.removeChild(li);
-    }
+    // function removeFormField(element) {
+    //     var form = document.getElementById("myForm");
+    //     var ul = form.querySelector("ul");
+    //     var li = element.parentNode;
+    //     ul.removeChild(li);
+    // }
 // do podglądu obrazu
 //scieżka ma byc z value z wybranego elementu
 // let sciezka = '';
