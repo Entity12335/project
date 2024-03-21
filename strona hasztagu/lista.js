5fc028f8fd4df92a6a7cb8f67a626f812de99cfc
PaKre66 <pawel.pazdur0@gmail.com>
2024-03-21 22:36:49 +0100

    już działa

--- strona hasztagu/lista.js
+++ strona hasztagu/lista.js
@@ -12,16 +12,36 @@
     // section.appendChild(newA);
     // };
 
+// function addHashtagElement() {
+//     var newHashtag = document.createElement("a");
+//     newHashtag.setAttribute("href", "../strona prepisu/index.html");
+//     newHashtag.classList.add("hasztag");
+//     newHashtag.textContent = "hasz";  
+//     var section = document.querySelector("article");
+//     section.appendChild(newHashtag);
+// }
+// function addButtonClick() {
+//     addHashtagElement();
+// }
+// var button = document.querySelector("przycisk");
+// button.addEventListener("click", addButtonClick);
+
 function addHashtagElement() {
+ 
     var newHashtag = document.createElement("a");
     newHashtag.setAttribute("href", "../strona prepisu/index.html");
     newHashtag.classList.add("hasztag");
-    newHashtag.textContent = "hasz";  
-    var section = document.querySelector("article");
-    section.appendChild(newHashtag);
+    newHashtag.textContent = "hasz"; 
+
+    var section = document.querySelector(".hasz");
+
+    if (section) {
+        section.appendChild(newHashtag);
+    } else {
+        console.error("Sekcja o klasie 'hasz' nie została znaleziona.");
+    }
 }
-function addButtonClick() {
-    addHashtagElement();
-}
-var button = document.querySelector("przycisk");
-button.addEventListener("click", addButtonClick);
+
+var button = document.getElementById("przycisk");
+
+button.addEventListener("click", addHashtagElement);
